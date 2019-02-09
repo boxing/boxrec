2b42af56145d9dc28dec8848ff0c2aabe8945e31
Mike DiDomizio <mikedidomizio@gmail.com>
2019-02-08 21:06:19 -0500

    watch done
    venue done
    
    titles done
    
    title done
    
    search done
    
    schedule done
    
    ratings done
    
    profile boxer done
    
    profile manager done

--- src/boxrec-pages/profile/boxrec.page.profile.constants.ts
+++ src/boxrec-pages/profile/boxrec.page.profile.constants.ts
@@ -1,34 +1,42 @@
 import {Location} from "@boxrec-constants";
 import {WeightDivision} from "@boxrec-pages/champions/boxrec.champions.constants";
 import {BoxrecPageProfileBoxerBoutRow} from "@boxrec-pages/profile/boxrec.page.profile.boxer.bout.row";
+import {BoxrecPageProfileManagerBoxerRow} from "@boxrec-pages/profile/boxrec.page.profile.manager.boxer.row";
 import {BoxrecProfileRole} from "@boxrec-pages/profile/boxrec.profile.constants";
 
-export interface BoxrecProfileBoxerOutput {
-    KOs: number | null;
-    alias: string | null;
+interface BoxrecProfileOutput {
     birthName: string | null;
     birthPlace: Location | null;
+    globalId: number | null;
+    name: string;
+    otherInfo: string[][];
+    picture: string;
+    residence: Location | null;
+    role: BoxrecProfileRole[];
+    status: string | null;
+}
+
+export interface BoxrecProfileBoxerOutput extends BoxrecProfileOutput {
+    KOs: number | null;
+    alias: string | null;
     born: string | null;
     bouts: BoxrecPageProfileBoxerBoutRow[];
     debut: string | null;
     division: WeightDivision | null;
-    globalId: number | null;
     hasBoutScheduled: boolean;
     height: number[] | null;
-    name: string;
     nationality: string | null;
     numberOfBouts: number;
-    otherInfo: string[][];
-    picture: string;
     ranking: number[][] | null;
     rating: number | null;
     reach: number[] | null;
-    residence: Location | null;
-    role: BoxrecProfileRole[];
     rounds: number | null;
     stance: string | null;
-    status: string | null;
     suspended: string | null;
     titlesHeld: string[] | null;
     vadacbp: boolean | null;
+}
+
+export interface BoxrecProfileManagerOutput extends BoxrecProfileOutput {
+    boxers: BoxrecPageProfileManagerBoxerRow[];
 }
