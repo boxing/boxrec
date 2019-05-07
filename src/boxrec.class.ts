63eb6c9ba24224c332f53368203063b35fc72240
Mike DiDomizio <mikedidomizio@gmail.com>
2019-05-07 19:16:23 -0400

    Clean up todos

--- src/boxrec.class.ts
+++ src/boxrec.class.ts
@@ -35,6 +35,7 @@ import {BoxrecPageTitles} from "./boxrec-pages/titles/boxrec.page.titles";
 import {BoxrecPageVenue} from "./boxrec-pages/venue/boxrec.page.venue";
 import {BoxrecPageWatch} from "./boxrec-pages/watch/boxrec.page.watch";
 import {BoxrecPageWatchRow} from "./boxrec-pages/watch/boxrec.page.watch.row";
+import {BoxrecPageLocationBoxer} from "./boxrec-pages/location/people/boxrec.page.location.boxer";
 
 // https://github.com/Microsoft/TypeScript/issues/14151
 if (typeof (Symbol as any).asyncIterator === "undefined") {
