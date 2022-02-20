b734c27b046878dfa30328cae5976f77c9319ce5
Mike DiDomizio <mikedidomizio@gmail.com>
2022-02-20 10:45:51 -0500

    Fix belt titles

---
+++ src/__tests__/boxrec.getTitleById.spec.e2e.ts
@@ -0,0 +1,98 @@
+import {CookieJar} from "request";
+import {BoxrecPageTitle} from "../boxrec-pages/title/boxrec.page.title";
+import {BoxrecPageTitlesRow} from "../boxrec-pages/titles/boxrec.page.titles.row";
+import {Boxrec} from "../boxrec.class";
+import {expectMatchDate, logIn, wait} from "./helpers";
+import DoneCallback = jest.DoneCallback;
+
+jest.unmock("request-promise");
+
+jest.setTimeout(30000);
+
+describe("method getTitleById", () => {
+
+    let loggedInCookie: CookieJar;
+
+    beforeAll(async (done: DoneCallback) => {
+        const logInResponse: { madeRequest: boolean, cookieJar: CookieJar} = await logIn();
+        loggedInCookie = logInResponse.cookieJar;
+
+        if (logInResponse.madeRequest) {
+            wait(done);
+        } else {
+            done();
+        }
+    });
+
+    const WBCMiddleweightEndpoint: string = "6/Middleweight";
+    let WBCMiddleweightResult: BoxrecPageTitle;
+
+    beforeAll(async () => {
+        WBCMiddleweightResult = await Boxrec.getTitleById(loggedInCookie, WBCMiddleweightEndpoint);
+    });
+
+    describe("getter name", () => {
+
+        it("should return the name of the title", () => {
+            expect(WBCMiddleweightResult.name).toBe("World Boxing Council World Middleweight Title");
+        });
+
+    });
+
+    describe("getter champion", () => {
+
+        it("should return the name and id of current champion", () => {
+            expect(WBCMiddleweightResult.champion).toEqual({
+                id: jasmine.any(Number),
+                name: jasmine.any(String),
+            });
+        });
+
+    });
+
+    describe("getter numberOfBouts", () => {
+
+        it("should return the number of bouts that have occurred for this title", () => {
+            expect(WBCMiddleweightResult.numberOfBouts).toBeGreaterThanOrEqual(111);
+        });
+
+    });
+
+    describe("getter bouts", () => {
+
+        it("should return an array of bouts that occurred for this title", () => {
+            expect(WBCMiddleweightResult.bouts).toEqual(jasmine.any(Array));
+        });
+
+        describe("bout values", () => {
+
+            let mostRecentWBCBout: BoxrecPageTitlesRow;
+
+            beforeAll(() => {
+                mostRecentWBCBout = WBCMiddleweightResult.bouts[0];
+            });
+
+            it("should include the date", () => {
+                expectMatchDate(mostRecentWBCBout.date);
+            });
+
+            it("should include the name and id of the first boxer", () => {
+                expect(mostRecentWBCBout.firstBoxer.id).not.toBeNull();
+                expect(mostRecentWBCBout.firstBoxer.name).not.toBeNull();
+            });
+
+            it("should include the name and id of the second boxer", () => {
+                expect(mostRecentWBCBout.secondBoxer.id).not.toBeNull();
+                expect(mostRecentWBCBout.secondBoxer.name).not.toBeNull();
+            });
+
+            it("should include the number of rounds", () => {
+                expect(mostRecentWBCBout.numberOfRounds[0]).toBeGreaterThan(0);
+                expect(mostRecentWBCBout.numberOfRounds[1]).toBeGreaterThan(0);
+            });
+
+        });
+
+    });
+
+});
