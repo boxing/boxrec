11733f95dd834ac13c09ceccaa1a07f907edd519
Mike DiDomizio <mikedidomizio@gmail.com>
2022-02-20 11:10:13 -0500

    Separate getResults e2e tests

--- src/boxrec.class.spec.e2e.ts
+++ src/boxrec.class.spec.e2e.ts
@@ -310,35 +310,6 @@ describe("class Boxrec (E2E)", () => {
 
     });
 
-    describe("method getResults", () => {
-
-        let results: BoxrecPageSchedule;
-        let nextResults: BoxrecPageSchedule;
-
-        beforeAll(async () => {
-            results = await Boxrec.getResults(loggedInCookie, {});
-            // note: replace the following if have a reason to grab different schedule data
-            nextResults = await Boxrec.getResults(loggedInCookie, {}, 20);
-        });
-
-        it("should give an array of events", () => {
-            expect(results.events.length).toBeGreaterThanOrEqual(0);
-        });
-
-        it("should use the `offset` to give the next results", async () => {
-            expect(results.events[0].id).not.toEqual(nextResults.events[0].id);
-        });
-
-        describe("getter numberOfPages", () => {
-
-            it("should return the number of pages", () => {
-                expect(results.numberOfPages).toBeGreaterThan(0);
-            });
-
-        });
-
-    });
-
     describe("method getRatings", () => {
 
         let ratings: BoxrecPageRatings;
